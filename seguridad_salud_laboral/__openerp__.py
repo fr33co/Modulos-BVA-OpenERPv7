51723d8fb821e7b09040fbb9377b6487bb0ec422
Angel Guadarrama <aguadarrama@plataforma.(none)>
2014-01-27 12:56:09 -0430

    ADD - Se añade el modulo para Seguridad y Salud Laboral

---
+++ seguridad_salud_laboral/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name' : "Unidad de Seguridad y Salud Laboral",
+    'category' : "Others",
+    'version' : "1.0",
+    'depends' : ['base', 'hr_holidays'],
+    'author' : "Angel A. Guadarrama B.",
+    'description' : '''\
+        Herencias para agregar campos al modulo hr_holidays
+     ''',
+    'data' : [
+            "views/ussl_view.xml",
+            ],
+}
