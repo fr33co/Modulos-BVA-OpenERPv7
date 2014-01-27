d3c7b03aa9a3a84689140290cbc574a74680138d
Angel Guadarrama <aguadarrama@plataforma.(none)>
2014-01-27 13:51:53 -0430

    ADD - Se añade atributos para los campos name_diseases_type_id y name_diseases_id

--- seguridad_salud_laboral/models/ussl.py
+++ seguridad_salud_laboral/models/ussl.py
@@ -6,7 +6,17 @@ class hr_holidays(osv.Model):
     _inherit = "hr.holidays"
     
     _columns = {
+        'name_diseases_type_id' : fields.many2one('hr.holidays.diseases.types', string="Tipo de Enfermedad", change_default=True, select=True, ondelete='cascade', required=False),
         'name_diseases_id' : fields.many2one('hr.holidays.diseases', string="Enfermedad", change_default=True, select=True, ondelete='cascade', required=False),
+    }
+
+class hr_holidays_diseases_types(osv.Model):
+    _name = "hr.holidays.diseases.types"
+    _order = "type_diseases"
+    _rec_name = "type_diseases"
+    
+    _columns = {
+        'type_diseases' : fields.char(string="Tipo de Enfermedad", required=True),
     }
 
 class hr_holidays_diseases(osv.Model):
@@ -19,11 +29,3 @@ class hr_holidays_diseases(osv.Model):
         'name_diseases_type_id' : fields.many2one('hr.holidays.diseases.types', string="Tipo de Enfermedad", required=True)
     }
     
-class hr_holidays_diseases_types(osv.Model):
-    _name = "hr.holidays.diseases.types"
-    _order = "type_diseases"
-    _rec_name = "type_diseases"
-    
-    _columns = {
-        'type_diseases' : fields.char(string="Tipo de Enfermedad", required=True),
-    }
