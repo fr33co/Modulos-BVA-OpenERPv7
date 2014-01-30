dbf0d1499420b35e447808f084b0df562f0e58f1
Angel Guadarrama <aguadarrama@plataforma.(none)>
2014-01-30 16:24:03 -0430

    FIX - Agregado a modulo gestion_canaimas la dependencia faltante para la topologia l10n_ve_topology

--- gestion_canaimas/__openerp__.py
+++ gestion_canaimas/__openerp__.py
@@ -3,7 +3,7 @@
     'name' : "Solicitud y Soporte Técnico de Canaimitas",
     'category' : "Others",
     'version' :  "1.0",
-    'depends' : ['base'],
+    'depends' : ['base', 'l10n_ve_topology'],
     'autor' : "Marcel Arcuri, Angel Guadarrama & A.C. Bibliotecas Virtuales de Aragua", 
     'description' : """\
         Modulo para la gestión de las solicitudes y soporte técnico para Canaimitas""",
