bf558775a7d8367f24e0c2c7b5ba6872995a01a5
Bianca Cuba <jdaponte@debian.(none)>
2014-06-19 08:42:41 -0430

    NUEVO MODULO DE GESTION DE EVENTOS 19/06/2014

---
+++ gestion_eventos/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import gestion_eventos # Gestion de Eventos
+import institucion_gerencia # Institucion / Gerencia
+import gestion_documento # Gestion de Documentos (Adjuntos)
+import recurso_logistico # Recurso Logistico
+import usuario_gerencia # Usuario ligado a la Gerencia
+import gestion_reportes # Generacion de Reportes para Eventos (Filtros)
